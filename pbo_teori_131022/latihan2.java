4c357b0e4fab1723430d8f03ba53ab41a6ae7820
Nindya Armon <nindyamori516@gmail.com>
2022-10-27 20:21:48 +0700

    Create latihan2.java

---
+++ pbo_teori_131022/latihan2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori_131022;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan2 {
+    public static void main(String[] args) {
+        
+        int hasil = 0;
+        
+        if (hasil % 2==0){
+            System.out.println("Bilangan Genap");
+        }
+        else{
+            System.out.println("Bilangan Ganjil");  
+        }
+        
+        System.out.print("Masukkan bilangan = ");
+    }
+}
