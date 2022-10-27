66a0e2afb8ab17fc485496c42cbfdf7c96add4e7
Nindya Armon <nindyamori516@gmail.com>
2022-10-27 20:17:56 +0700

    Create latihan3.java

---
+++ nindy071022/latihan3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nindy071022;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan3 {
+    public static void main(String[] args) {
+        int grade = 92;
+        
+        switch (grade) {
+            case 100:
+                System.out.println("Excelent!");
+                break;
+            case 90:
+                System.out.println("Good Job!");
+                break;
+            case 80:
+                System.out.println("Study Harder!");
+                break;
+            default:
+                System.out.println("Sorry, you failed");
+        }
+    }
+    
+}
