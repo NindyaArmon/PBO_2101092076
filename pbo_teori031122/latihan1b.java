eb191aea21588e65428c22fc80cc134c8b14e6fe
Nindya Armon <nindyamori516@gmail.com>
2022-11-03 22:14:20 +0700

    Create latihan1b.java

---
+++ pbo_teori031122/latihan1b.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori031122;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan1b {
+    public static void main(String[] args) {
+        String[] hari = {"Senin","Selasa","Kamis","Jumat","Sabtu","Minggu"};
+        int i = 0;
+        
+        do{
+            System.out.println(hari[i]);
+            i++;
+        }while(i<hari.length);
+    }
+}
