8adfc2cf52033b1311ead03b727dc774ab32f51d
Nindya Armon <nindyamori516@gmail.com>
2022-11-03 22:15:09 +0700

    Create latihan3b.java

---
+++ pbo_teori_271022/latihan3b.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori_271022;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan3b {
+    public static void main(String[] args) {
+        int r=0;
+        do{
+            System.out.println("nindya");
+            r++;
+        }while(true);
+    }  
+}
