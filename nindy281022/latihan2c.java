9b69a8c423d5f5fa795f6fcaf46b545510225b9b
Nindya Armon <nindyamori516@gmail.com>
2022-11-03 22:13:45 +0700

    Create latihan2c.java

---
+++ nindy281022/latihan2c.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nindy281022;
+
+/**
+ *
+ * @author hp
+ */
+public class latihan2c {
+        public static void main(String[] args) {
+        String hari [] ={"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+        
+        for (int i=0; i < hari.length; i++){
+        System.out.println(hari[i]);
+        }
+    }
+}
