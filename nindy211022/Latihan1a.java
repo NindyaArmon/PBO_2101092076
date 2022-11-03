793310b08e34751d6dcca55624f2c2da66048650
Nindya Armon <nindyamori516@gmail.com>
2022-11-03 22:13:09 +0700

    Create Latihan1a.java

---
+++ nindy211022/Latihan1a.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nindy211022;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hp
+ */
+public class Latihan1a {
+    public static void main(String[] args) {
+        int r=0;
+        while(r < 100){
+            System.out.println("nindya");
+            r++;
+        }
+    }
+}
