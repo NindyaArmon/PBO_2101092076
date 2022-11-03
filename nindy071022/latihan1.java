54f6a41e9a0eadee53d4c813dd082f40d757bbea
Nindya Armon <nindyamori516@gmail.com>
2022-11-03 22:11:57 +0700

    Update latihan1.java

--- nindy071022/latihan1.java
+++ nindy071022/latihan1.java
@@ -1,31 +1,28 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nindy071022;
+package pbo_teori_271022;
 
 /**
  *
  * @author hp
  */
 public class latihan1 {
-    public static void main(String[] args) {
-        double grade;
-        grade = 92.0;
-        
-        if (grade >= 92.0){
-            System.out.println("Excelent!");
+    public static void main(String[] args){
+        double grade = 92.0;
+        if(grade >= 90){
+            System.out.println("Excellent!!");
         }
-        else if ((grade < 90) && (grade >= 80)) {
-            System.out.println("Good Job!");
+        else if((grade < 90) && (grade >= 80)){
+            System.out.println("Good Job!!");
         }
-        else if ((grade < 80) && (grade >= 60)) {
-            System.out.println("Study Harder!");
+        else if((grade < 80) && (grade >= 60)){
+            System.out.println("Study Harder!!");
         }
-        else {
-            System.out.println("Sorry, you faloled.");
+        else{
+            System.out.println("Sorry, you failed!!");
         }
     }
-   
 }
