c53c5c0b83525274e37a443a1a63f41994493bed
Nindya Armon <nindyamori516@gmail.com>
2022-12-02 12:20:31 +0700

    Create StudentRecord.java

---
+++ nindy021222/StudentRecord.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nindy021222;
+
+/**
+ *
+ * @author hp
+ */
+public class StudentRecord {
+    public static void main(String[] args) {
+        
+    }
+}
