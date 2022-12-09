49b7bd2550c89067f7e64266963ca55ff38da2cb
Nindya Armon <nindyamori516@gmail.com>
2022-12-09 11:53:23 +0700

    Create PolimorfisExample.java

---
+++ nindy091222/PolimorfisExample.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nindy091222;
+
+/**
+ *
+ * @author hp
+ */
+public class PolimorfisExample {
+    public static void main (String [] args ){
+        Person ref;
+        Student student = new Student();
+        Employee employee = new Employee();
+        ref = student;
+        
+        String temp = ref.getName();
+        System.out.println("Name " + temp);
+        
+        ref = employee;
+        String temp1 = ref.getName();
+        System.out.println("" + temp1);
+        
+    
+    }
+}
