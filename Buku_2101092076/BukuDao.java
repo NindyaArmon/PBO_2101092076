3fc2f2af34a8353d87ca7a457653aa032508ad5f
Nindya Armon <nindyamori516@gmail.com>
2023-01-26 20:15:43 +0700

    Create BukuDao.java

---
+++ Buku_2101092076/BukuDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buku_2101092076;
+
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+public interface BukuDao {
+    Buku save (Buku anggota);
+    Buku update(int index, Buku buku);
+    void delete (int index);
+    Buku getBuku(int index);
+    List<Buku> getAllBuku();
+}
