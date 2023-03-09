6a995e49fc02dc514d0391fe412f4a2a9b21784e
Nindya Armon <nindyamori516@gmail.com>
2023-03-09 14:47:39 +0700

    Create Coba.java

---
+++ pbo_teori_081222/Coba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori_081222;
+
+/**
+ *
+ * @author hp
+ */
+public class Coba {
+    public static void main(String[] args) {
+        string name;
+    }
+    
+    String getName(){
+    
+    }
+}
